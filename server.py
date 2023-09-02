b448c25e80da121468e62d05c8e052990fae3e25
Pranav Jadhav <pranavj@pal-nat186-57-199.itap.purdue.edu>
2023-09-02 01:27:42 -0400

    Added import for downloader

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from gensim.models.keyedvectors import KeyedVectors
 
 path = "/Users/pranavj/gensim-data/word2vec-google-news-300/word2vec-google-news-300.gz"
-model = KeyedVectors.load_word2vec_format(path, binary=True)
-print("done loading")
 
 # To download the model, uncomment the following lines:
 # DO NOT COMMIT THESE LINES UNCOMMENTED
+# import gensim.downloader as api
 # path = api.load("word2vec-google-news-300", return_path=True)
 # print(path)
 
+model = KeyedVectors.load_word2vec_format(path, binary=True)
+print("done loading")
 
 
 # print(model.similarity("cat","cinnamon"))
